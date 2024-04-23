68ecd4b61f836505e51157979bf5141c1036d2d4
Jeison Gonzalez Manuyama <jeisonmanuyama@gmail.com>
2024-04-23 03:45:30 -0500

    modelos de las entidades

---
+++ app/Models/Empleado.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    use HasFactory;
+
+    protected $table = 'empleados';
+    protected $fillable = ['nombre', 'apellido', 'posición', 'departamento_id', 'fecha_contratación', 'salario'];
+
+    public function departamento()
+    {
+        return $this->belongsTo(Departamento::class);
+    }
+
+    public function asistencias()
+    {
+        return $this->hasMany(Asistencia::class);
+    }
+}
