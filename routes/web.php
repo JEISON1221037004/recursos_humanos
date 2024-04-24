f68ae146bffff978e421df5bc3bc8817445db0c0
Jeison Gonzalez Manuyama <jeisonmanuyama@gmail.com>
2024-04-24 00:39:24 -0500

    funcionsmiento de autenticacion y algunos cambios en las vistas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\AsistenciaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rutas para Empleados
+    Route::resource('empleados', EmpleadoController::class);
+
+    // Rutas para Departamentos
+    Route::resource('departamentos', DepartamentoController::class);
+
+    // Rutas para Asistencias
+    Route::resource('asistencias', AsistenciaController::class);
+});
+
+require __DIR__.'/auth.php';
