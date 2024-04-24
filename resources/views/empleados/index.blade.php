6eb1ea88a31fbdefa12c4f3132bbb99b1afc5af1
Jeison Gonzalez Manuyama <jeisonmanuyama@gmail.com>
2024-04-24 01:12:27 -0500

    Otros cambios cambios en las vistas

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -1,32 +1,44 @@
 {{-- resources/views/empleados/index.blade.php --}}
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <h1>Empleados</h1>
-    <a href="{{ route('empleados.create') }}" class="btn btn-primary">Agregar Empleado</a>
-    <table class="table table-bordered">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Lista de Empleados</title>
+</head>
+<body>
+<div class="container mt-4">
+    <h1 class="mb-4">Lista de Empleados</h1>
+    <div class="mb-3">
+        <a href="{{ route('empleados.create') }}" class="btn btn-success">Agregar Empleado</a>
+    </div>
+    <table class="table">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Posición</th>
-                <th>Departamento</th>
-                <th>Acciones</th>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Posición</th>
+                <th scope="col">Departamento</th>
+                <th scope="col">Acciones</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($empleados as $empleado)
             <tr>
-                <td>{{ $empleado->id }}</td>
+                <th scope="row">{{ $empleado->id }}</th>
                 <td>{{ $empleado->nombre }}</td>
                 <td>{{ $empleado->apellido }}</td>
                 <td>{{ $empleado->posición }}</td>
                 <td>{{ $empleado->departamento->nombre }}</td>
                 <td>
-                    <a href="{{ route('empleados.edit', $empleado->id) }}" class="btn btn-warning">Editar</a>
-                    <form action="{{ route('empleados.destroy', $empleado->id) }}" method="POST" style="display:inline">
+                    <a href="{{ route('empleados.edit', $empleado->id) }}" class="btn btn-info">Editar</a>
+                    <form action="{{ route('empleados.destroy', $empleado->id) }}" method="POST" style="display: inline-block">
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger">Eliminar</button>
@@ -37,4 +49,12 @@
         </tbody>
     </table>
 </div>
-@endsection
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
