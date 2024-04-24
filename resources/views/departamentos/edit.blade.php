af966ac0cd34665799e75333a7fadfb231c93088
Jeison Gonzalez Manuyama <jeisonmanuyama@gmail.com>
2024-04-24 03:12:40 -0500

    nuevos arreglos para departamentos

--- resources/views/departamentos/edit.blade.php
+++ resources/views/departamentos/edit.blade.php
@@ -1,7 +1,18 @@
-{{-- resources/views/departamentos/edit.blade.php --}}
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous">
+
+    <title>Editar Departamento</title>
+</head>
+<body>
 <div class="container">
     <h1>Editar Departamento</h1>
     <form action="{{ route('departamentos.update', $departamento->id) }}" method="POST">
@@ -19,7 +30,15 @@
             <label for="número_telefono" class="form-label">Número de Teléfono</label>
             <input type="text" class="form-control" id="número_telefono" name="número_telefono" value="{{ $departamento->número_telefono }}" required>
         </div>
-        <button type="submit" class="btn btn-success">Actualizar</button>
+        <button type="submit" class="btn btn-primary">Actualizar</button>
+        <a href="{{ route('departamentos.index') }}" class="btn btn-warning">Cancelar</a>
     </form>
 </div>
-@endsection
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- resources/views/departamentos/index.blade.php
+++ resources/views/departamentos/index.blade.php
@@ -1,11 +1,20 @@
-{{-- resources/views/departamentos/index.blade.php --}}
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Listado de Departamentos</title>
+</head>
+<body>
 <div class="container">
-    <h1>Departamentos</h1>
-    <a href="{{ route('departamentos.create') }}" class="btn btn-primary">Agregar Departamento</a>
-    <table class="table table-bordered">
+    <h1>Listado de Departamentos</h1>
+    <a href="{{ route('departamentos.create') }}" class="btn btn-success">Agregar Departamento</a>
+    <table class="table">
         <thead>
             <tr>
                 <th>ID</th>
@@ -23,7 +32,7 @@
                 <td>{{ $departamento->ubicación }}</td>
                 <td>{{ $departamento->número_telefono }}</td>
                 <td>
-                    <a href="{{ route('departamentos.edit', $departamento->id) }}" class="btn btn-warning">Editar</a>
+                    <a href="{{ route('departamentos.edit', $departamento->id) }}" class="btn btn-info">Editar</a>
                     <form action="{{ route('departamentos.destroy', $departamento->id) }}" method="POST" style="display:inline">
                         @csrf
                         @method('DELETE')
@@ -35,4 +44,12 @@
         </tbody>
     </table>
 </div>
-@endsection
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
